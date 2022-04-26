2bebe3dd528c6eb554d8a747ebafb7ab527823c7
Mathias Lieber <lieber@bitnetic.de>
2022-04-26 09:17:35 +0200

    Format release (fulfil Stringer interface)

--- catalog/release.go
+++ catalog/release.go
@@ -1,6 +1,7 @@
 package catalog
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -26,4 +27,22 @@ type ReleaseHistory struct {
 	Vendor   string
 	Product  string
 	Releases []Release
+}
+
+func (r *Release) String() string {
+	productName := fmt.Sprintf("%s %s", r.Vendor, r.Product)
+	if r.Variant != "" {
+		productName = fmt.Sprintf("%s %s", productName, r.Variant)
+	}
+
+	arch := ""
+	if r.OS != "" || r.Arch != "" {
+		sep := ""
+		if r.OS != "" && r.Arch != "" {
+			sep = ", "
+		}
+		arch = fmt.Sprintf(" (%s%s%s)", r.OS, sep, r.Arch)
+	}
+
+	return fmt.Sprintf("%s%s %s, released on %s", productName, arch, r.Version, r.Date)
 }
