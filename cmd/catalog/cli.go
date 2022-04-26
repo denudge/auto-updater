439dd2490628b99c18db8b6019e045fa60e4784c
Mathias Lieber <lieber@bitnetic.de>
2022-04-26 12:16:03 +0200

    Avoid pre-existing release info on fractional seconds

--- cmd/catalog/cli.go
+++ cmd/catalog/cli.go
@@ -36,25 +36,43 @@ func (app *App) createReleaseCommands() *cli.Command {
 		Subcommands: []*cli.Command{
 			{
 				Name:  "latest",
-				Usage: "List recently published releases",
+				Usage: "list recently published releases",
 				Action: func(c *cli.Context) error {
 					return app.ListLatestReleases()
 				},
 			},
 			{
 				Name:  "publish",
-				Usage: "Publish new release",
-				Action: func(c *cli.Context) error {
+				Usage: "publish new release",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "vendor", Usage: "Vendor name"},
+					&cli.StringFlag{Name: "product", Usage: "Product name"},
+					&cli.StringFlag{Name: "variant", Usage: "Variant (Pro, Free, ...)"},
+					&cli.StringFlag{Name: "version", Usage: "Version in semantic versioning scheme"},
+					&cli.StringFlag{Name: "os", Usage: "Operating system (MacOS, darwin, linux, ...)"},
+					&cli.StringFlag{Name: "arch", Usage: "Architecture (i686, ppc64, ...)"},
+					&cli.BoolFlag{Name: "unstable", Usage: "Mark release as unstable"},
+				},
+				Before: func(c *cli.Context) error {
+					// Check arguments
+					if c.String("vendor") == "" || c.String("product") == "" || c.String("version") == "" {
+						_ = cli.ShowCommandHelp(c, "publish")
 
-					// TODO: Read values from arguments
+						return fmt.Errorf("At least vendor, product and version must be specified.\n")
+					}
+
+					return nil
+				},
+				Action: func(c *cli.Context) error {
 					release := &catalog.Release{
-						Vendor:  "Foo",
-						Product: "Bar",
-						Variant: "Pro",
-						OS:      "",
-						Arch:    "",
-						Version: "1.1.0",
-						Date:    time.Now(),
+						Vendor:   c.String("vendor"),
+						Product:  c.String("product"),
+						Variant:  c.String("variant"),
+						OS:       c.String("osName"),
+						Arch:     c.String("arch"),
+						Version:  c.String("version"),
+						Date:     time.Now(),
+						Unstable: c.Bool("unstable"),
 					}
 
 					stored, err := app.store.Store(release, false)
@@ -62,7 +80,8 @@ func (app *App) createReleaseCommands() *cli.Command {
 						return err
 					}
 
-					if stored.Date.Before(release.Date) {
+					// time.Time.Before() cannot be used because the database might drop fractional seconds
+					if stored.Date.Unix() < release.Date.Unix() {
 						fmt.Println("Release has already been there.")
 					}
 
