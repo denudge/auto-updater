949d1e1f8cbb4b42dbfa4f23243c136924a7ee00
Mathias Lieber <lieber@bitnetic.de>
2022-04-26 09:44:30 +0200

    Refined release formatting

--- catalog/release.go
+++ catalog/release.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Release struct {
-	Vendor        string
-	Product       string
-	Name          string
-	Variant       string
+	Vendor        string // must be present and match a client's installation
+	Product       string // must be present and match a client's installation
+	Name          string // for printing; if not given, "<vendor> <product>" will be used
+	Variant       string // sth like "Pro" or "Light" (if given it must match a client's installation)
 	Description   string
-	OS            string
-	Arch          string
+	OS            string // sth like "MacOS", "darwin" or "linux"; if given it must match a client's installation
+	Arch          string // sth like "i386 or "ppc64"; if given it must match a client's installation
 	Date          time.Time
 	Version       string // for use with semantic versioning
-	Unstable      bool
-	Alias         string
+	Unstable      bool   // some clients may not want to use unstable versions
+	Alias         string // sth like "Focal Fossa"; optional, for printing (and release management) only
 	Signature     string // a cryptographical representation (hash etc)
 	Tags          []string
 	UpgradeTarget UpgradeTarget // If empty, the default upgrade target will be used
@@ -30,7 +30,11 @@ type ReleaseHistory struct {
 }
 
 func (r *Release) String() string {
-	productName := fmt.Sprintf("%s %s", r.Vendor, r.Product)
+	productName := r.Name
+	if productName == "" {
+		productName = fmt.Sprintf("%s %s", r.Vendor, r.Product)
+	}
+
 	if r.Variant != "" {
 		productName = fmt.Sprintf("%s %s", productName, r.Variant)
 	}
@@ -44,5 +48,14 @@ func (r *Release) String() string {
 		arch = fmt.Sprintf(" (%s%s%s)", r.OS, sep, r.Arch)
 	}
 
-	return fmt.Sprintf("%s%s %s, released on %s", productName, arch, r.Version, r.Date)
+	version := r.Version
+	if r.Alias != "" {
+		version = fmt.Sprintf("%s (\"%s\")", r.Version, r.Alias)
+	}
+
+	if r.Unstable {
+		version = fmt.Sprintf("%s [unstable]", version)
+	}
+
+	return fmt.Sprintf("%s%s, version %s, released on %s", productName, arch, version, r.Date)
 }
