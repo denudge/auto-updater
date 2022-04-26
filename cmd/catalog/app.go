efb51c0d161ecb723b3a4ef640f5ed65f279d47b
Mathias Lieber <lieber@bitnetic.de>
2022-04-26 10:20:11 +0200

    Generalize app container

--- cmd/catalog/app.go
+++ cmd/catalog/app.go
@@ -10,18 +10,24 @@ import (
 
 type App struct {
 	db    *bun.DB
-	store *database.DbCatalogStore
+	store catalog.StoreInterface
 }
 
 func NewApp(db *bun.DB, ctx context.Context) *App {
 	return &App{
 		db:    db,
-		store: database.NewDbCatalogStore(db, context.Background()),
+		store: database.NewDbCatalogStore(db, ctx),
 	}
 }
 
 func (app *App) ListLatestReleases() error {
-	latest, err := app.store.LatestReleases(10)
+	dbStore, ok := app.store.(*database.DbCatalogStore)
+	if !ok {
+		fmt.Printf("Cannot print latest releases")
+		return nil
+	}
+
+	latest, err := dbStore.LatestReleases(10)
 	if err != nil {
 		return err
 	}
