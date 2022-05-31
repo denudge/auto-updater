6678649e7b94adb697565348794873a52a65cb4c
Mathias Lieber <lieber@bitnetic.de>
2022-05-31 09:14:58 +0200

    Changes to the catalog interface

--- catalog/catalog.go
+++ catalog/catalog.go
@@ -1,9 +1,10 @@
 package catalog
 
 type Catalog interface {
-	ShouldUpgrade(state ClientState) (Criticality, error)
+	RegisterClient(vendor string, product string, variant string) (*ClientState, error)
+	ShouldUpgrade(state *ClientState) (Criticality, error)
 	// FindNextUpgrade should return nil if no update is available
-	FindNextUpgrade(state ClientState) (*UpgradeStep, error)
+	FindNextUpgrade(state *ClientState) (*UpgradeStep, error)
 	// FindUpgradePath should return nil if no update is available
-	FindUpgradePath(state ClientState) (*UpgradePath, error)
+	FindUpgradePath(state *ClientState) (*UpgradePath, error)
 }
